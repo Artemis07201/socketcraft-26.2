6bedb42264a8a29963fc51a1041c75bdac55d3be
Artemis07201 <artemis07201@gmail.com>
2026-07-31 23:13:32 -0500

    gems and textures

--- src/main/java/net/stellarartemis/socketcraft/SocketCraftDataGenerator.java
+++ src/main/java/net/stellarartemis/socketcraft/SocketCraftDataGenerator.java
@@ -2,10 +2,15 @@ package net.stellarartemis.socketcraft;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.stellarartemis.socketcraft.provider.EnglishLangProvider;
+import net.stellarartemis.socketcraft.provider.ModelProvider;
 
 public class SocketCraftDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(EnglishLangProvider::new);
+		pack.addProvider(ModelProvider::new);
 	}
 }
